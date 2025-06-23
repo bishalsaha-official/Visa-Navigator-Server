b89e72454746addd894009dbbacf83528c40b9cc
bishalsaha-official <cse.bishalsaha@gmail.com>
2025-06-24 01:02:39 +0600

    Implement Post method for add visa

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json());
+require('dotenv').config()
 
 app.get('/', (req, res) => {
   res.send('Visa Navigator Is Running Successfully')
 })
+
+// Mongo DB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jxyklbs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const visaCollection = client.db("VisaDb").collection("visas");
+
+    // Post Visa From Add visa Component
+    app.post('/visas', async(req, res)=>{
+      const newVisa = req.body
+      const result = await visaCollection.insertOne(newVisa);
+      res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
   console.log(`Visa Navigator Server Running on port ${port}`)
