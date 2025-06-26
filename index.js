a5ef3b7dea1850d6d616d6ddff921cdec35ab752
bishalsaha-official <cse.bishalsaha@gmail.com>
2025-06-26 23:36:29 +0600

    Modify applied visa functionality

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ async function run() {
       res.send(result)
     })
 
+    // Delete applied visa item
     app.delete('/appliedvisa/:id', async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) };
